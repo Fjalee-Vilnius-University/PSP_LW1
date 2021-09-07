45db5cab8382fdf0fbde9e91c22e252e34858363
Rytis Cepulis <cepRytis@gmail.com>
2021-09-07 22:51:43 +0300

    phone numebr validator

--- PSP_LW1-UnitTests/PhoneValidatorTests.cs
+++ PSP_LW1-UnitTests/PhoneValidatorTests.cs
@@ -1,13 +1,51 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PSP_LW1;
 
 namespace PSP_LW1_UnitTests
 {
     [TestClass]
     public class PhoneValidatorTests
     {
+        readonly PhoneValidator _passwordChecker = new PhoneValidator(8, "+370", "8");
+        readonly string _correctPhoneWithoutPrefix = "69675098";
+
         [TestMethod]
-        public void TestMethod1()
+        public void IsValid_ValidWithDefaultPrefix_Incorrect()
         {
+            var phone = "+370" + _correctPhoneWithoutPrefix;
+
+            var result = _passwordChecker.IsValid(phone);
+
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void IsValid_ValidWithShortcutPrefix_Incorrect()
+        {
+            var phone = "8" + _correctPhoneWithoutPrefix;
+
+            var result = _passwordChecker.IsValid(phone);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsValid_HasLetters_Incorrect()
+        {
+            var phone = "8685AA695";
+
+            var result = _passwordChecker.IsValid(phone);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void IsValid_TooShort_Incorrect()
+        {
+            var phone = "86855695";
+
+            var result = _passwordChecker.IsValid(phone);
+
+            Assert.IsFalse(result);
         }
     }
 }
--- PSP_LW1/PhoneValidator.cs
+++ PSP_LW1/PhoneValidator.cs
@@ -2,5 +2,22 @@
 {
     public class PhoneValidator
     {
+        readonly int _lenWithoutPrefix;
+        readonly string _defaultPrefix;
+        readonly string _shortcutPrefix;
+
+        public PhoneValidator(int lenWithoutPrefix, string defaultPrefix, string shortcutPrefix)
+        {
+            _lenWithoutPrefix = lenWithoutPrefix;
+            _defaultPrefix = defaultPrefix;
+            _shortcutPrefix = shortcutPrefix;
+        }
+
+        public bool IsValid(string phone)
+        {
+            return true;
+        }
+
+
     }
 }
