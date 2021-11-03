07cd0f9202e3a960e11cf124d131fc30ef36db0a
Rytis Cepulis <cepRytis@gmail.com>
2021-11-03 23:12:42 +0200

    added api project for user creation

---
+++ PSP_collegues_library/PSP2021/UserCreationAPI/Controllers/UserCreationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserCreationAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserCreationController : ControllerBase
+    {
+        [HttpGet()]
+        public void Test()
+        {
+            var temp = 0;
+        }
+    }
+}
