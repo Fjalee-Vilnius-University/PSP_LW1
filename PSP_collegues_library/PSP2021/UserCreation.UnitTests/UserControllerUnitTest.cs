932cdfd46655ee44274498addb57dd78d261bc94
Rytis Cepulis <cepRytis@gmail.com>
2021-11-07 22:06:43 +0200

    delete user endpoint unit

--- PSP_collegues_library/PSP2021/UserCreation.UnitTests/UserControllerUnitTest.cs
+++ PSP_collegues_library/PSP2021/UserCreation.UnitTests/UserControllerUnitTest.cs
@@ -150,5 +150,53 @@ namespace UserCreation.UnitTests
             Assert.NotNull(resultStatusCode);
             Assert.Equal(500, resultStatusCode.StatusCode);
         }
+
+
+        [Theory]
+        [AutoDomainData]
+        public void DeleteUser_Ok(UserDto user, [Frozen] Mock<IUserService> userService, [Greedy] UserController sut)
+        {
+            userService
+                .Setup(e => e.DeleteUser(It.IsAny<int>()))
+                .Returns(user);
+
+            var result = sut.DeleteUser(It.IsAny<int>());
+            var resultStatusCode = result as OkObjectResult;
+            var content = resultStatusCode.Value;
+
+            Assert.NotNull(resultStatusCode);
+            Assert.Equal(200, resultStatusCode.StatusCode);
+            Assert.Equal(content, user);
+        }
+
+        [Theory]
+        [AutoDomainData]
+        public void DeleteUser_BadRequest([Frozen] Mock<IUserService> userService, [Greedy] UserController sut)
+        {
+            userService
+                .Setup(e => e.DeleteUser(It.IsAny<int>()))
+                .Returns((UserDto)null);
+
+            var result = sut.DeleteUser(It.IsAny<int>());
+            var resultStatusCode = result as BadRequestResult;
+
+            Assert.NotNull(resultStatusCode);
+            Assert.Equal(400, resultStatusCode.StatusCode);
+        }
+
+        [Theory]
+        [AutoDomainData]
+        public void DeleteUser_IntertalError([Frozen] Mock<IUserService> userService, [Greedy] UserController sut)
+        {
+            userService
+                .Setup(e => e.DeleteUser(It.IsAny<int>()))
+                .Throws(new Exception());
+
+            var result = sut.DeleteUser(It.IsAny<int>());
+            var resultStatusCode = result as StatusCodeResult;
+
+            Assert.NotNull(resultStatusCode);
+            Assert.Equal(500, resultStatusCode.StatusCode);
+        }
     }
 }
